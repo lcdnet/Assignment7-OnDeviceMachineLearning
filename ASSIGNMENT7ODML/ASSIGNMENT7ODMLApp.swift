7fc42a782a8f0159b4ef7a81366a4c9c81d9d73c
ooney <62164814+ooneykabooey@users.noreply.github.com>
2026-06-28 21:47:30 -0500

    Initial Commit

---
+++ ASSIGNMENT7ODML/ASSIGNMENT7ODMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASSIGNMENT7ODMLApp.swift
+//  ASSIGNMENT7ODML
+//
+//  Created by Levi Daniel on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ASSIGNMENT7ODMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASSIGNMENT7ODML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASSIGNMENT7ODML
+//
+//  Created by Levi Daniel on 6/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
